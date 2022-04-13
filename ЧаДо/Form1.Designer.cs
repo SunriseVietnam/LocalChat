02218d6864b462b0c15e18447c13df1d4173c1e1
Sunrice <tropin.nd@edu.spbstu.ru>
2022-04-13 21:21:08 +0300

    Добавьте файлы проекта.

---
+++ ЧаДо/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿using System.Windows.Forms;
+
+namespace ЧаДо
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.logoutButton = new System.Windows.Forms.Button();
+            this.sendButton = new System.Windows.Forms.Button();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.messageTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.saveTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.userNameTextBox.Location = new System.Drawing.Point(90, 12);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.Size = new System.Drawing.Size(146, 20);
+            this.userNameTextBox.TabIndex = 0;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(242, 12);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(90, 20);
+            this.loginButton.TabIndex = 1;
+            this.loginButton.Text = "Вход";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.loginButton_Click);
+            // 
+            // logoutButton
+            // 
+            this.logoutButton.Location = new System.Drawing.Point(242, 38);
+            this.logoutButton.Name = "logoutButton";
+            this.logoutButton.Size = new System.Drawing.Size(90, 20);
+            this.logoutButton.TabIndex = 2;
+            this.logoutButton.Text = "Выход";
+            this.logoutButton.UseVisualStyleBackColor = true;
+            this.logoutButton.Click += new System.EventHandler(this.logoutButton_Click);
+            // 
+            // sendButton
+            // 
+            this.sendButton.Location = new System.Drawing.Point(257, 219);
+            this.sendButton.Name = "sendButton";
+            this.sendButton.Size = new System.Drawing.Size(75, 50);
+            this.sendButton.TabIndex = 3;
+            this.sendButton.Text = "Отправить";
+            this.sendButton.UseVisualStyleBackColor = true;
+            this.sendButton.Click += new System.EventHandler(this.sendButton_Click);
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.BackColor = System.Drawing.SystemColors.Menu;
+            this.chatTextBox.Location = new System.Drawing.Point(12, 64);
+            this.chatTextBox.Multiline = true;
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.ReadOnly = true;
+            this.chatTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.chatTextBox.Size = new System.Drawing.Size(320, 149);
+            this.chatTextBox.TabIndex = 4;
+            // 
+            // messageTextBox
+            // 
+            this.messageTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.messageTextBox.Location = new System.Drawing.Point(12, 219);
+            this.messageTextBox.Multiline = true;
+            this.messageTextBox.Name = "messageTextBox";
+            this.messageTextBox.Size = new System.Drawing.Size(239, 50);
+            this.messageTextBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Введите имя:";
+            // 
+            // saveTimer
+            // 
+            this.saveTimer.Enabled = true;
+            this.saveTimer.Tick += new System.EventHandler(this.saveTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(344, 281);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.messageTextBox);
+            this.Controls.Add(this.chatTextBox);
+            this.Controls.Add(this.sendButton);
+            this.Controls.Add(this.logoutButton);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.userNameTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Чат для Дома:ЧаДо";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox userNameTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Button logoutButton;
+        private System.Windows.Forms.Button sendButton;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.TextBox messageTextBox;
+        private System.Windows.Forms.Label label1;
+        private Timer saveTimer;
+    }
+}
+
---
+++ ЧаДо/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Media;
+using ЧаДо;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+namespace ЧаДо
+{
+    [Serializable]
+    public partial class Form1 : Form
+    {
+        BinaryFormatter binFormatter = new BinaryFormatter();
+        bool sound = true;
+        bool alive = false; // будет ли работать поток для приема
+        UdpClient client;
+        const int LOCALPORT = 8001; // порт для приема сообщений
+        const int REMOTEPORT = 8001; // порт для отправки сообщений
+        const int TTL = 20;
+        const string HOST = "235.5.5.11"; // хост для групповой рассылки
+        IPAddress groupAddress; // адрес для групповой рассылки
+        string userName; // имя пользователя в чате
+        public Form1()
+        {
+            InitializeComponent();
+
+            loginButton.Enabled = true; // кнопка входа
+            logoutButton.Enabled = false; // кнопка выхода
+            sendButton.Enabled = false; // кнопка отправки
+            chatTextBox.ReadOnly = true; // поле для сообщений
+
+            
+            groupAddress = IPAddress.Parse(HOST);
+            try
+            {
+                using (var file = new FileStream("users.bin", FileMode.OpenOrCreate))
+                {
+                    var User = binFormatter.Deserialize(file) as string;
+                    if (User != null)
+                    {
+                        userNameTextBox.Text = User;
+                    }
+                }
+            }
+            catch { }
+            try
+            {
+                using (var file = new FileStream("dialogs.bin", FileMode.Open))
+                {
+                    var dialogs = binFormatter.Deserialize(file) as string;
+                    if (dialogs != null)
+                    {
+                        chatTextBox.Text = dialogs;
+                    }
+                }
+            }
+            catch  { }
+        }
+        // обработчик нажатия кнопки loginButton
+        private void loginButton_Click(object sender, EventArgs e)
+        {
+            userName = userNameTextBox.Text;
+            userNameTextBox.ReadOnly = true;
+
+            using(var file = new FileStream("users.bin",FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(file,userName);
+            }
+                try
+                {
+                    client = new UdpClient(LOCALPORT);
+                    // присоединяемся к групповой рассылке
+                    client.JoinMulticastGroup(groupAddress, TTL);
+
+                    // запускаем задачу на прием сообщений
+                    Task receiveTask = new Task(ReceiveMessages);
+                    receiveTask.Start();
+
+                    // отправляем первое сообщение о входе нового пользователя
+                    sound = false;
+                    string message = userName + " вошел в чат";
+                    byte[] data = Encoding.Unicode.GetBytes(message);
+                    client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                    loginButton.Enabled = false;
+                    logoutButton.Enabled = true;
+                    sendButton.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+        }
+        // проигрывание звука нового сообщения
+        static void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(Resource1.icq_message);
+            simpleSound.Play();
+        }
+        // метод приема сообщений
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+                    // добавляем полученное сообщение в текстовое поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        chatTextBox.Text = time + " " + message + "\r\n" + chatTextBox.Text;
+                        if (sound) playSimpleSound();
+                        else sound = true;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // обработчик нажатия кнопки sendButton
+        public void sendButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sound = false;
+                string message = String.Format("{0}: {1}", userName, messageTextBox.Text);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                messageTextBox.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        // обработчик нажатия кнопки logoutButton
+        private void logoutButton_Click(object sender, EventArgs e)
+        {
+            sound = false;
+            ExitChat();
+        }
+        // выход из чата
+        private void ExitChat()
+        {
+            string message = userName + " покидает чат";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, HOST, REMOTEPORT);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+            client.Close();
+
+            loginButton.Enabled = true;
+            logoutButton.Enabled = false;
+            sendButton.Enabled = false;
+            userNameTextBox.ReadOnly = false;
+        }
+        // обработчик события закрытия формы
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (alive)
+                ExitChat();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && sendButton.Enabled && !String.IsNullOrWhiteSpace(messageTextBox.Text)) sendButton_Click(this, new EventArgs());
+            if (String.IsNullOrWhiteSpace(messageTextBox.Text)) messageTextBox.Clear();
+        }
+
+        private void saveTimer_Tick(object sender, EventArgs e)
+        {
+            using (var file = new FileStream("dialogs.bin",FileMode.OpenOrCreate))
+            {
+               binFormatter.Serialize(file,chatTextBox.Text);
+            }
+        }
+    }
+}
